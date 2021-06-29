d384b4c98756b00b4900b88018daafd43ae53605
Gitlab_test_project <letellier.henry@gmail.com>
2021-06-29 10:22:51 +0200

    updated the stylization sheets

--- files/customisation/js/dark_light.js
+++ files/customisation/js/dark_light.js
@@ -1,9 +1,14 @@
 function theme(ID = "darkOrLight", ID2 = "theme") {
     var WasChecked = document.getElementById(ID).checked,
         home = "https://hanralatalliardwork.github.io/wolf_escape_home/";
-    if (WasChecked === true) {
+    if (WasChecked === false) {
         document.getElementById(ID2).innerHTML = "<link rel=\"stylesheet\" href=\"" + home + "files/customisation/css/bootstrap.css\">";
     } else {
-        document.getElementById(ID2).innerHTML = "<link rel=\"stylesheet\" href=\"" + home + "files/customisation/css/bootstrap_dark.css\">";
+        document.getElementById(ID2).innerHTML = "<link rel=\"stylesheet\" href=\"" + home + "files/customisation/css/boostrap_dark.css\">";
     }
+}
+
+function initialiseTheme(ID) {
+    var home = "https://hanralatalliardwork.github.io/wolf_escape_home/";
+    document.getElementById(ID).innerHTML = "<link rel=\"stylesheet\" href=\"" + home + "files/customisation/css/bootstrap.css\">";
 }
