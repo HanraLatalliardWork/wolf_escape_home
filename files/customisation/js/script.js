5df993279dcf1499107dcaeb08bc6dcad697ed4d
Hanra <37299738+HenraL@users.noreply.github.com>
2021-07-26 11:11:19 +0200

    worked on the desing of the page

--- files/customisation/js/script.js
+++ files/customisation/js/script.js
@@ -8,3 +8,16 @@ function showHide(IDFrom,IDTo,IDText){
         document.getElementById(IDText).innerHTML="Show";
     }
 }
+function checkTheme(CookieName,ID1,ID2,ID3){
+    currentTheme=readCookie(CookieName);
+    if (currentTheme==="L" | currentTheme==="l"){
+        document.getElementById(ID1).innerHTML="<img class=\"image_WE_Link_Container_child\" src=\"files/img/down-arrow.svg\" alt=\"Download Image\" title=\"Download Image\"/>";
+        document.getElementById(ID2).innerHTML="<img class=\"image_credits\" src=\"files/img/down-arrow.svg\"/>";
+        document.getElementById(ID3).innerHTML="<img class=\"image_Discord_Link_Container_child\" src=\"files/requirements/img/Follow-me/discord/discord_B.PNG\" alt=\"Discord logo\" title=\"Discord logo\"/>";
+    } else {
+        document.getElementById(ID1).innerHTML="<img class=\"image_WE_Link_Container_child\" src=\"files/img/down-arrow_W.svg\" alt=\"Download Image\" title=\"Download Image\"/>";
+        document.getElementById(ID2).innerHTML="<img class=\"image_credits\" src=\"files/img/down-arrow_W.svg\"/>";
+        document.getElementById(ID3).innerHTML="<img class=\"image_Discord_Link_Container_child\" src=\"files/requirements/img/Follow-me/discord/discord_W.PNG\" alt=\"Discord logo\" title=\"Discord logo\"/>";
+    }
+}
+checkTheme("theme","DownloadLogo","DownloadButtonCredits","DiscordLogo")
