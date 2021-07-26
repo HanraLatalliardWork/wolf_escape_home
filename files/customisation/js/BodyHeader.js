e1d0cdcbafc50b64d3dd35c604b6319d91a367e7
Hanra <37299738+HenraL@users.noreply.github.com>
2021-07-27 01:28:53 +0200

    Put the header in a js

---
+++ files/customisation/js/BodyHeader.js
@@ -0,0 +1,33 @@
+function writeBodyHeader(ID){
+    var writeBodyHeader="";
+    writeBodyHeader+='    <HeaderContainer>\n';
+    writeBodyHeader+='    <HeaderLeft>\n';
+    writeBodyHeader+='        <a class="link" href="https://hanralatalliardwork.github.io/wolf_escape_home/"><img class="homeIcon" src="files/img/Home/wolf_Icon_home.png"/></a>\n';
+    writeBodyHeader+='    </HeaderLeft>\n';
+    writeBodyHeader+='    <center class="HeaderCenter">\n';
+    writeBodyHeader+='        <h1>Wolf Escape</h1>\n';
+    writeBodyHeader+='    </center>\n';
+    writeBodyHeader+='    <HeaderRight>\n';
+    writeBodyHeader+='        <div class="fb-like" data-href="https://www.facebook.com/HenrySoftwareHouse" data-width="" data-layout="button_count" data-action="like" data-size="small" data-share="true"></div>\n';
+    writeBodyHeader+='        <!-- Place this tag where you want the button to render. -->\n';
+    writeBodyHeader+='        <a class="github-button" href="https://github.com/hanralatalliardwork" data-color-scheme="no-preference: light; light: light; dark: dark;" data-show-count="true" aria-label="Follow @hanralatalliardwork on GitHub">Follow @hanralatalliardwork</a>\n';
+    writeBodyHeader+='        <!-- Place this tag where you want the button to render. -->\n';
+    writeBodyHeader+='        <a class="github-button" href="https://github.com/hanralatalliardwork/wolf_escape_home/subscription" data-icon="octicon-eye" data-show-count="true" aria-label="Watch hanralatalliardwork/wolf_escape_home on GitHub">Watch</a>\n';
+    writeBodyHeader+='        <!-- Place this tag where you want the button to render. -->\n';
+    writeBodyHeader+='        <a class="github-button" href="https://github.com/hanralatalliardwork/wolf_escape_home" data-icon="octicon-star" data-show-count="true" aria-label="Star hanralatalliardwork/wolf_escape_home on GitHub">Star</a>\n';
+    writeBodyHeader+='        <!-- Place this tag where you want the button to render. -->\n';
+    writeBodyHeader+='        <a class="github-button" href="https://github.com/hanralatalliardwork/My_styling/fork" data-icon="octicon-repo-forked" data-show-count="true" aria-label="Fork hanralatalliardwork/My_styling on GitHub">Fork</a>\n';
+    writeBodyHeader+='    </HeaderRight>\n';
+    writeBodyHeader+='</HeaderContainer>\n';
+    writeBodyHeader+='<div id="checkboxContainer">\n';
+    writeBodyHeader+='    <input type="checkbox" id="darkOrLight" onclick="theme(\'darkOrLight\',\'theme\',\'https://hanralatalliardwork.github.io/wolf_escape_home/\',\'files/customisation/css/boostrap_dark.css\',\'files/customisation/css/bootstrap.css\');checkTheme(\'theme\',\'DownloadLogo\',\'DownloadButtonCredits\',\'DiscordLogo\');">\n';
+    writeBodyHeader+='    <label for="darkOrLight">\n';
+    writeBodyHeader+='        <div id="star">\n';
+    writeBodyHeader+='            <div class="star" id="star-1">&starf;</div>\n';
+    writeBodyHeader+='            <div class="star" id="star-2">&starf;</div>\n';
+    writeBodyHeader+='        </div>\n';
+    writeBodyHeader+='        <div id="moon"></div>\n';
+    writeBodyHeader+='    </label>\n';
+    writeBodyHeader+='</div>\n';
+    document.getElementById(ID).innerHTML=writeBodyHeader;
+}
