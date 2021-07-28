256edd0adccf0858ea90acc9b4987f0e2218f39a
Hanra <37299738+HenraL@users.noreply.github.com>
2021-07-28 14:51:21 +0200

    Worked on making some themes portable across the pages

--- files/level_creators/available_decorations/js/script.js
+++ files/level_creators/available_decorations/js/script.js
@@ -47,3 +47,22 @@ function initialiseMenus() {
         document.getElementById(AllTheIDS[i]).style.display = "none";
     }
 }
+
+function UpdateTheme(){
+    var CookieName="theme",
+    listOfIDsToEdit=["DownloadLogo","DownloadButtonCredits","DiscordLogo","GithubLogo"],
+    DictOfCodesToApply={
+        "l":{
+            "DownloadLogo":"<img class=\"image_WE_Link_Container_child\" src=\"files/img/down-arrow.svg\" alt=\"Download Image\" title=\"Download Image\"/>",
+            "DownloadButtonCredits":"<img class=\"image_credits\" src=\"files/img/down-arrow.svg\"/>",
+            "DiscordLogo":"<img class=\"image_Discord_Link_Container_child\" src=\"files/requirements/img/Follow-me/discord/discord_B.PNG\" alt=\"Discord logo\" title=\"Discord logo\"/>",
+            "GithubLogo":"<img class=\"image_hosts\" src=\"files/requirements/img/Follow-me/github/github_B.PNG\"/>"
+        },"d":{
+            "DownloadLogo":"<img class=\"image_WE_Link_Container_child\" src=\"files/img/down-arrow_W.svg\" alt=\"Download Image\" title=\"Download Image\"/>",
+            "DownloadButtonCredits":"<img class=\"image_credits\" src=\"files/img/down-arrow_W.svg\"/>",
+            "DiscordLogo":"<img class=\"image_Discord_Link_Container_child\" src=\"files/requirements/img/Follow-me/discord/discord_W.png\" alt=\"Discord logo\" title=\"Discord logo\"/>",
+            "GithubLogo":"<img class=\"image_hosts\" src=\"files/requirements/img/Follow-me/github/github_W.png\"/>"
+        }
+    };
+    checkTheme(CookieName,listOfIDsToEdit,DictOfCodesToApply);
+}
