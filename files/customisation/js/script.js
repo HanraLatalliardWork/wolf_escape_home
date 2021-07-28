027ee86a8f667c357d4d00131838affb18686ee3
Hanra <37299738+HenraL@users.noreply.github.com>
2021-07-28 12:52:34 +0200

    moved the theme to a new file

--- files/customisation/js/script.js
+++ files/customisation/js/script.js
@@ -8,12 +8,11 @@ function showHide(IDFrom,IDTo,IDText){
         document.getElementById(IDText).innerHTML="Show";
     }
 }
-function checkTheme(CookieName,listOfIDsToEdit,DictOfCodesToApply){
-    currentTheme=readCookie(CookieName).toLowerCase();
-    console.log("currentTheme="+currentTheme);
-    console.log("CookieName="+CookieName);
-    for (var i=0;i<listOfIDsToEdit.length;i++){
-        document.getElementById(listOfIDsToEdit[i]).innerHTML=DictOfCodesToApply[currentTheme][listOfIDsToEdit[i]];
-    }
-    
-}
+// function checkTheme(CookieName,listOfIDsToEdit,DictOfCodesToApply){
+//     currentTheme=readCookie(CookieName).toLowerCase();
+//     console.log("currentTheme="+currentTheme);
+//     console.log("CookieName="+CookieName);
+//     for (var i=0;i<listOfIDsToEdit.length;i++){
+//         document.getElementById(listOfIDsToEdit[i]).innerHTML=DictOfCodesToApply[currentTheme][listOfIDsToEdit[i]];
+//     }
+// }
