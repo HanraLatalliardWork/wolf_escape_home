57f4432323b920361f58957a164d63027240ed35
Hanra <37299738+HenraL@users.noreply.github.com>
2021-07-28 15:28:11 +0200

    cleaning scripts

--- files/customisation/js/script.js
+++ files/customisation/js/script.js
@@ -1,13 +1,13 @@
-function showHide(IDFrom,IDTo,IDText){
-    e=document.getElementById(IDFrom).checked;
-    if (e===true){
-        document.getElementById(IDTo).style.display="block";
-        document.getElementById(IDText).innerHTML="Hide";
-    } else {
-        document.getElementById(IDTo).style.display="none";
-        document.getElementById(IDText).innerHTML="Show";
-    }
-}
+// function showHide(IDFrom,IDTo,IDText){
+//     e=document.getElementById(IDFrom).checked;
+//     if (e===true){
+//         document.getElementById(IDTo).style.display="block";
+//         document.getElementById(IDText).innerHTML="Hide";
+//     } else {
+//         document.getElementById(IDTo).style.display="none";
+//         document.getElementById(IDText).innerHTML="Show";
+//     }
+// }
 // function checkTheme(CookieName,listOfIDsToEdit,DictOfCodesToApply){
 //     currentTheme=readCookie(CookieName).toLowerCase();
 //     console.log("currentTheme="+currentTheme);
